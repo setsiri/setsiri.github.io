fb7b02d1b11a586d38502c772522669eaa7bd088
setsiri matewin <setsiri_matewin@cmu.ac.th>
2024-11-19 11:34:35 +0700

    push

--- app/resume/page.js
+++ app/resume/page.js
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
       <div>
         <h1>My Resume</h1>
-        <p>This is the Resume page where you can see my professional experience.</p>
+        <p>This is the Resume page where you can see my professional experience.testd</p>
       </div>
     );
   }
